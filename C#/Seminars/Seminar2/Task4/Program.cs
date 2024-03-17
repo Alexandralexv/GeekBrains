ceb0a5f8a9d8786c60dcddb00d5278358dbdb3f6
Alexandr <alexandralexv@gmail.com>
2024-03-17 14:17:56 +0300

    Seminar2:Task4

--- C#/Seminars/Seminar2/Task4/Program.cs
+++ C#/Seminars/Seminar2/Task4/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью с
+// конца цифру заданного числа или сообщает, что
+// третьей цифры нет.
+// 456 => 6
+// 7812 => 1
+// 91 => Третьей цифры нет
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+
+    Console.WriteLine(number % 10);
+
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
