cb4a2c061b977271bf9459962c66af928fd69bbc
Alexandr <alexandralexv@gmail.com>
2024-03-17 10:20:10 +0300

    Task2

--- Seminars/Seminar1/Task2/Program.cs
+++ Seminars/Seminar1/Task2/Program.cs
@@ -8,8 +8,9 @@ Console.WriteLine("Введите целое число");
 int N = Convert.ToInt32(Console.ReadLine());
 int i = -N;
 
-while (i <= N)
+while (i < N)
 {
     Console.Write(i + ", ");
     i++;
 }
+Console.Write(i);
