84fe01c497e16e615b5ab4cc7b5f04d100095b36
Alexandr <alexandralexv@gmail.com>
2024-03-17 10:40:20 +0300

    Task3

---
+++ Seminars/Seminar1/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное целое число и на выходе показывает сумму
+// первой и последней цифры этого числа.
+// 456 => 10
+// 782 => 9
+// 918 => 17
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number % 10 + number / 100;
+
+Console.WriteLine(result);
