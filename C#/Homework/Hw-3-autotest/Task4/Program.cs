9bcfd55cc4c727c511802cdb481f9b826851d318
Alexandr <alexandralexv@gmail.com>
2024-03-23 13:15:31 +0300

    Hw-3-autotest:Task4

--- C#/Homework/Hw-3-autotest/Task4/Program.cs
+++ C#/Homework/Hw-3-autotest/Task4/Program.cs
@@ -8,3 +8,36 @@
 // 8741 => [8 7 4 1]
 // 4 => [4]
 
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0)
+{
+    int memory_number = number;
+    int k = 0;
+
+    while (number > 0) // Находим кол-во символов в числе
+    {
+        number /= 10;
+        k++;
+    }
+
+    int[] arr = new int[k];
+
+    for (int i = 0; i < k; i++)
+    {
+        arr[i] = memory_number % 10;
+        memory_number /= 10;
+    }
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+else
+{
+    Console.WriteLine("Некорректный ввод");
+}
+
+
+
+
