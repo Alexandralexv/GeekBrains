6c910a93f4c8370e0beb68370f1a4f1b64ebf69e
Alexandr <alexandralexv@gmail.com>
2024-03-23 10:58:58 +0300

    Seminar3-arrays:Task1

--- C#/Seminars/Seminar3-arrays/Task1/Program.cs
+++ C#/Seminars/Seminar3-arrays/Task1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив. Напишите программу, которая определяет,
+// присутствует ли заданное число в массиве. Программа
+// должна выдать ответ: Да/Нет.
+// Примеры
+// [1 3 4 19 3], 8 => Нет
+// [-4 3 4 1], 3 => Да 
+
+int[] arr = {1, 3, 8, 19, 3, 5};
+
+int num = 8;
+bool res = false;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] == num)
+    {
+        res = true;
+        break;
+    }
+}
+
+// if (res == true)
+// {
+//     Console.WriteLine($"{num} => Да");
+// }
+// else
+// {
+//     Console.WriteLine($"{num} => Нет");
+// }
+
+Console.WriteLine(res ? $"{num} => Да" : $"{num} => Нет");
+
