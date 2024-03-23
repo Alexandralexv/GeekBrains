62bda67ac19d0439b53544140460ea92967e00d3
Alexandr <alexandralexv@gmail.com>
2024-03-23 11:52:54 +0300

    Seminar3-arrays:Task4

--- C#/Seminars/Seminar3-arrays/Task4/Program.cs
+++ C#/Seminars/Seminar3-arrays/Task4/Program.cs
@@ -5,3 +5,25 @@
 // 456 => [6 5 4]
 // 781 => [1 8 7]
 
+Console.WriteLine("Введите трёхзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100 || number > 999)
+{
+    Console.WriteLine("Некорректный ввод");
+}
+else
+{
+    int[] result = new int[3];
+
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = number % 10;
+        number /= 10;
+    }
+
+    for (int i = 0; i < result.Length; i++)
+    {
+        Console.Write(result[i] + " ");
+    }
+}
