a848ea67fd61ad5a635e6628f21877c911f38dff
Alexandr <alexandralexv@gmail.com>
2024-03-24 15:23:45 +0300

    Hw-4-selftest:Task1

--- C#/Homework/Hw-4-selftest/Task1/Program.cs
+++ C#/Homework/Hw-4-selftest/Task1/Program.cs
@@ -5,3 +5,33 @@
 // 5 12 16 q [STOP]
 // 3 45 342 15 [STOP]
 
+void EndlessNumberRequest()
+{
+    while (true)
+    {
+        Console.WriteLine("Введите целое число(для выхода введите: q)");
+        string symbol = Console.ReadLine();
+        if (symbol == "q" || SumIsEven(Convert.ToInt32(symbol)))
+        {
+            return;
+        }
+    }
+}
+
+bool SumIsEven(int num)
+{
+    bool variable = false;
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += num % 10;
+        num = num / 10;
+    }
+    if (sum % 2 == 0)
+    {
+        variable = true;
+    }
+    return variable;
+}
+
+EndlessNumberRequest();
