c3bde2b54e622cb36bf6b1638055dff1ea0a3d59
Alexandr <alexandralexv@gmail.com>
2024-03-31 17:34:43 +0300

    Hw-5-autotest:Task3

--- C#/Homework/Hw-5-autotest/Task3/Program.cs
+++ C#/Homework/Hw-5-autotest/Task3/Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Задайте прямоугольный двумерный массив.
+// Напишите программу, которая будет находить строку с
+// наименьшей суммой элементов.
+// 4 3 1 => Строка с индексом 0
+// 2 6 9
+// 4 6 2 
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],3} ");
+        }
+        Console.WriteLine("]");
+    }
+
+}
+
+int[] PrintSumOfRows(int[,] matrix)
+{
+    int[] arr = new int[matrix.GetLength(1)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        arr[i] = sum;
+    }
+    return arr;
+
+}
+
+void OutputSmallestNumber(int[] array)
+{
+    int min = array[0];
+    int index = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (min > array[i])
+        {
+            min = array[i];
+            index = i;
+        }
+    }
+    Console.WriteLine($"Cтрока с индексом {index}");
+}
+
+int[,] arr2d = CreateMatrixRndInt(3, 3, 1, 10);
+PrintMatrix(arr2d);
+Console.WriteLine();
+int[] arr = PrintSumOfRows(arr2d);
+OutputSmallestNumber(arr);
