89d2bed5c61d07be54727b3883a3372ae7012b22
Alexandr <alexandralexv@gmail.com>
2024-04-02 16:45:00 +0300

    Hw-6-selftest:Task4

--- C#/Homework/Hw-6-selftest/Task4/Program.cs
+++ C#/Homework/Hw-6-selftest/Task4/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4*(не обязательная): Задайте строку, состоящую
+// из слов, разделенных пробелами. Сформировать строку,
+// в которой слова расположены в обратном порядке. В
+// полученной строке слова должны быть также разделены
+// пробелами.
+// “Hello my world” => “world my Hello”
+
+void SupplyReversal(string hello)
+{
+    string res = string.Empty;
+    string[] helloSplit = hello.Split();
+
+    for (int i = helloSplit.Length - 1; i >= 0; i--)
+    {
+        res += helloSplit[i] + " ";
+    }
+
+    Console.WriteLine(res);
+}
+
+string hello = "Hello my world";
+
+SupplyReversal(hello);
