e0e7b3e7d1c10f7efd7c96b51e350fe9b6af3c0c
Alexandr <alexandralexv@gmail.com>
2024-04-02 16:18:38 +0300

    Hw-6-selftest:Task2

--- C#/Homework/Hw-6-selftest/Task2/Program.cs
+++ C#/Homework/Hw-6-selftest/Task2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Задайте строку, содержащую латинские буквы
+// в обоих регистрах. Сформируйте строку, в которой все
+// заглавные буквы заменены на строчные. 
+// “aBcD1ef!-” => “abcd1ef!-” 
+
+string str = "aBcD1ef!-";
+
+str = str.ToLower();
+
+Console.WriteLine(str);
