f77648685a7b78dc155cd7d654e9563cddab0a97
Alexandr <alexandralexv@gmail.com>
2024-04-02 21:13:52 +0300

    DeBug

--- C#/Lectures/Lesson5-recursion/Task1/Program.cs
+++ C#/Lectures/Lesson5-recursion/Task1/Program.cs
@@ -6,9 +6,15 @@ int Fact(int n)
 {
     if (n == 1 || n == 0)
     {
+        Console.WriteLine($"Stop reqursion: n = {n}");
         return 1;
     }
+    Console.WriteLine(n);
     return n * Fact(n - 1);
 }
 
-Console.Write(Fact(0));
+Console.Write(Fact(5));
+
+//F11 шаг с заходом
+//F10 шаг с обходом
+//F5 продолжить (До след. точки)
