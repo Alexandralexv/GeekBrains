2ebff5b8d7652dbc8c53dca96dbe33a0f7ff8064
Alexandr <alexandralexv@gmail.com>
2024-04-02 15:20:45 +0300

    Seminar6-lines:Task3

--- C#/Seminars/Seminar6-lines/Task3/Program.cs
+++ C#/Seminars/Seminar6-lines/Task3/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Считать строку с консоли, состоящую из латинских
+// букв в нижнем регистре. Выяснить, сколько среди
+// введённых букв гласных.
+// Пример
+// “hello” => 2
+// “world” => 1
+
+
+int CountNumberVolwels(string str)
+{
+    string volwes = "aouyei";
+    int k = 0;
+    str = str.ToLower();
+    for (int i = 0; i < str.Length; i++)
+    {
+        for (int j = 0; j < volwes.Length; j++)
+        {
+            if (str[i] == volwes[j])
+            {
+                k++;
+            }
+        }
+    }
+    return k;
+}
+
+Console.WriteLine("Введите строку");
+string input = Console.ReadLine();
+
+int result = CountNumberVolwels(input);
+Console.WriteLine(result);
