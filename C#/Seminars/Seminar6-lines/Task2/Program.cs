8ce2e02e71661717c98f0451dce5becb4ffb8054
Alexandr <alexandralexv@gmail.com>
2024-04-02 15:08:17 +0300

    Seminar6-lines:Task2

--- C#/Seminars/Seminar6-lines/Task2/Program.cs
+++ C#/Seminars/Seminar6-lines/Task2/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// На основе символов строки (тип string) сформировать массив
+// символов (тип char[]). Вывести массив на экран.
+// Указание
+// Метод строки ToCharArray() не использовать.
+// Пример
+// “Hello!” => [‘H’, ‘e’, ‘l’, ‘l’, ‘o’, ‘!’ ] 
+void PrintArray(char[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}");
+        }
+    }
+    Console.Write("]");
+}
+
+static char[] StringToCharArray()
+{
+    string str = "Hello";
+
+    char[] result = new char[str.Length];
+
+    for (int i = 0; i < str.Length; i++)
+    {
+        result[i] = str[i];
+    }
+
+    return result;
+}
+
+char[] result = StringToCharArray();
+
+PrintArray(result);
+
