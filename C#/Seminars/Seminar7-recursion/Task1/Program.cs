ba38070e19ed4a3b71912c87e873996114b8f163
Alexandr <alexandralexv@gmail.com>
2024-04-06 10:59:56 +0300

    Seminar7-recursion:Task1

--- C#/Seminars/Seminar7-recursion/Task1/Program.cs
+++ C#/Seminars/Seminar7-recursion/Task1/Program.cs
@@ -5,7 +5,7 @@
 // Пример
 // N=5 => 1 2 3 4 5
 
-Console.WriteLine('Введите натуральное число');
+Console.WriteLine("Введите натуральное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 PrintNaturalNumbers(number);
@@ -13,6 +13,6 @@ PrintNaturalNumbers(number);
 void PrintNaturalNumbers(int num)
 {
     if(num == 0) return;
-    PrintNaturalNumbers(num -1);
+    PrintNaturalNumbers(num - 1);
     Console.Write($"{num} ");
 }
