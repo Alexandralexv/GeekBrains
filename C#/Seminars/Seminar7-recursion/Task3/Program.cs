a457c95846d6e5bae8849d94d541b6dd1e73d52e
Alexandr <alexandralexv@gmail.com>
2024-04-06 11:58:49 +0300

    Seminar7-recursion:Task3

--- C#/Seminars/Seminar7-recursion/Task3/Program.cs
+++ C#/Seminars/Seminar7-recursion/Task3/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Считать строку с консоли, содержащую латинские буквы.
+// Вывести на экран согласные буквы этой строки.
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+// Пример
+// “hello” => h l l
+// “World” => W r l d
+// “Hello world!” => H l l w r l d
+
+void PrintConsonants(string str, int count = 0)
+{
+    if (count == str.Length) return;
+    string vowels = "aouyie";
+    if (char.IsAsciiLetter(str[count]) == true && !vowels.Contains(char.ToLower(str[count])))
+    {
+        Console.Write(str[count] + " ");
+    }
+    PrintConsonants(str, count + 1);
+}
+
+void PrintConsonants2(string str)
+{
+    if (str.Length == 0) return;
+    string vowels = "aouyie";
+    if (char.IsAsciiLetter(str[0]) == true && !vowels.Contains(char.ToLower(str[0])))
+    {
+        Console.Write(str[0] + " ");
+    }
+    PrintConsonants2(str.Substring(1));
+}
+
+Console.WriteLine("Введите строку латинскими буквами");
+string userInput = Console.ReadLine();
+
+PrintConsonants(userInput);
+PrintConsonants2(userInput);
