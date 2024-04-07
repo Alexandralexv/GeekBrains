b438fc68810755866fd6db36cbe669480b2682bc
Alexandr <alexandralexv@gmail.com>
2024-04-07 15:52:44 +0300

    Hw-7-certification:Task2

--- C#/Homework/Hw-7-certification/Task2/Program.cs
+++ C#/Homework/Hw-7-certification/Task2/Program.cs
@@ -1,5 +1,24 @@
 ﻿// Задача 2: Напишите программу вычисления функции
 // Аккермана с помощью рекурсии. Даны два
 // неотрицательных числа m и n. 
-// m = 2, n = 3 -> A(m,n) = 29
-// m = 11, n = 3 -> A(m,n) = 16381
+// n = 3, m = 2 -> A(m,n) = 29
+// n = 3, m = 11 -> A(m,n) = 16381
+
+int FunctionOfAkkerman(int n, int m)
+{
+    if (n == 0)
+    {
+        return m + 1;
+    }
+    else if (m == 0)
+    {
+        return FunctionOfAkkerman(n - 1, 1);
+    }
+    else
+    {
+        return FunctionOfAkkerman(n - 1, FunctionOfAkkerman(n, m - 1));
+    }
+}
+
+int functionOfAkkerman = FunctionOfAkkerman(3, 11);
+Console.WriteLine(functionOfAkkerman);
