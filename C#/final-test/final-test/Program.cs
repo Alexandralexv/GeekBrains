4ac09a5c9277c7349f86b537fbff2d2f0b81c264
Alexandr <alexandralexv@gmail.com>
2024-04-28 14:01:11 +0300

    Create CreateNewArray function

--- C#/final-test/final-test/Program.cs
+++ C#/final-test/final-test/Program.cs
@@ -26,22 +26,22 @@ string[] CreateNewArray(string[] array)
     return newArray;
 }
 
-// void PrintArray(string[] array)
-// {
-//     Console.Write("[");
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         if (i < array.Length - 1)
-//         {
-//             Console.Write($"“{array[i]}”, ");
-//         }
-//         else
-//         {
-//             Console.Write($"“{array[i]}”");
-//         }
-//     }
-//     Console.WriteLine("]");
-// }
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"“{array[i]}”, ");
+        }
+        else
+        {
+            Console.Write($"“{array[i]}”");
+        }
+    }
+    Console.WriteLine("]");
+}
 
 // // Console.WriteLine(LinesLengthLessthree(arr));
 
